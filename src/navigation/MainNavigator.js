52d4618b90da79c79a3365fd985392b613c00d98
Sasindu Malhara <sasindumalhara@gmail.com>
2025-08-28 17:58:22 +0530

    Update MainNavigator.js

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -69,7 +69,7 @@ export default function MainNavigator({ user, onLogout }) {
           paddingBottom: 0,
           paddingTop: 0,
           height: 70,
-            marginBottom:50,
+            marginBottom:25,
             marginHorizontal:10,
             alignItems: 'space-between',
            // justifyContent: 'center',
